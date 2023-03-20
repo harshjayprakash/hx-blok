41358b0ffd32c758d502e0c0e5dc6649ed57d26c
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-03-20 09:00:57 +0000

    Updated code comments.

--- src/window.h
+++ src/window.h
@@ -25,56 +25,73 @@ typedef struct Window
 /**
  * Constructor for the Window structure.
  * @param app: A reference to the Application structure.
- * @returns 0 for 'success', 1 for 'self does not contain an instance of the
- *      window structure', 2 for 'app does not contain an instance of the
- *      application structure', 3 for 'failed to register window', 4 for 'failed
- *      to create window'.
+ * @returns
+ *      0 for 'success',
+ *      1 for 'self does not contain an instance of the window structure',
+ *      2 for 'app does not contain an instance of the application structure',
+ *      3 for 'failed to register window',
+ *      4 for 'failed to create window'.
 */
 int Window_Init(Window* self, Application* app);
 
 /**
  * Destructor for the Window structure. Frees up any memory allocated within
  * the Window structure.
- * @returns 0 for 'success', 1 for 'self does not contain an instance of the
- *      window structure.
+ * @returns
+ *      0 for 'success',
+ *      1 for 'self does not contain an instance of the window structure.
 */
 int Window_Del(Window* self);
 
 /**
  * Initialises and registers the extended version of the window class.
- * @returns Result of the RegisterClassEx Macro method. OR -1 for 'window class
- *      has already been registered'.
+ * @returns
+ *      Result of the RegisterClassEx Macro method. OR
+ *      -1 for 'self does not contain an instance of the window structure',
+ *      -2 for 'window class has already been registered'.
 */
 int Window_Register(Window* self);
 
 /**
  * Creates the window assigning it the HWND, the handle to the window.
- * @returns 0 for 'success', 1 for 'failed to create window', -1 for 'the window
- *      has already been registered', -2 for 'the window class has not been
- *      registered'.
+ * @returns
+ *      0 for 'success',
+ *      1 for 'failed to create window',
+ *      -1 for 'self does not contain an instance of the window structure',
+ *      -2 for 'the window has already been registered',
+ *      -3 for 'the window class has not been registered'.
 */
 int Window_Create(Window* self);
 
 /**
  * Shows the window created.
- * @returns 0 for 'success', -1 for 'the window has not been created', -2 for 
- *      'the window class has not been registered', -3 for 'the window is
- *      already visible'.
+ * @returns
+ *      0 for 'success',
+ *      -1 for 'self does not contain an instance of the window structure',
+ *      -2 for 'the window has not been created',
+ *      -3 for 'the window class has not been registered',
+ *      -4 for 'the window is already visible'.
 */
 int Window_Show(Window* self);
 
 /**
  * Updates the window.
- * @returns 0 for 'success', -1 for 'the window has not been created', -2 for 
- *      'the window class has not been registered'.
+ * @returns
+ *      0 for 'success',
+ *      -1 for 'self does not contain an instance of the window structure',
+ *      -2 for 'the window has not been created',
+ *      -3 fof 'the window class has not been registered'.
 */
 int Window_Update(Window* self);
 
 /**
  * Runs the message loop, a while loop that translates and dispatches window
  * messages including creating any controls.
- * @returns 0 for 'success', -1 for 'the window has not been created', -2 for 
- *      'the window class has not been registered'.
+ * @returns
+ *      0 for 'success',
+ *      -1 for 'self does not contain an instance of the window structure',
+ *      -2 for 'the window has not been created',
+ *      -3 for 'the window class has not been registered'.
 */
 int Window_MessageLoop(Window* self);
 
