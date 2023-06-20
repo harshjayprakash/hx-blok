21fbb8fb95f2ef4a509068cc4299027f7cef9575
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-20 09:17:11 +0100

    Updated main to include WINAPI Entry.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <windows.h>
 
-int main(void)
-{
-    printf("Hello, world\n");
+int APIENTRY WinMain(
+        HINSTANCE instance_handle, HINSTANCE previous_instance_handle, LPSTR command_line_arguments, 
+        int show_flag) {
     return 0;
 }
