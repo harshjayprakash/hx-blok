68427483921bd723163c050f9e97971e2b6ddf2f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-20 13:17:38 +0100

    Added function stubs.

---
+++ src/core/program.c
@@ -0,0 +1,19 @@
+#include "program.h"
+
+void program_init(
+    HINSTANCE _instance_handle, 
+    HINSTANCE _previous_instance_handle, 
+    LPSTR _command_line_arguments, 
+    int _show_flag)
+{
+
+}
+
+program *program_instance_get(void)
+{
+}
+
+void program_free(void)
+{
+
+}
