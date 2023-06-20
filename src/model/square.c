a7bbd882775fc7243856557be61dcc637e0b591a
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-20 13:50:20 +0100

    Added function implementation.

--- src/model/square.c
+++ src/model/square.c
@@ -2,20 +2,23 @@
 
 square square_new(int _x, int _y, int _width, int _height)
 {
-
+    square sq = { _x, _y, _width, _height };
+    return sq;
 }
 
 void square_position_set(square *sq, int _x, int _y)
 {
-
+    sq->x = _x;
+    sq->y = _y;
 }
 
 void square_size_set(square *sq, int _width, int _height)
 {
-
+    sq->width = _width;
+    sq->height = _height;
 }
 
 void square_free(square *sq)
 {
-    
+
 }
