9e12af2c055d0e62683fcdc4188878ab16136b70
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-20 13:44:41 +0100

    Removed the const declaration.

--- src/model/square.h
+++ src/model/square.h
@@ -11,10 +11,10 @@ typedef struct square__
 
 square square_new(int _x, int _y, int _width, int _height);
 
-void square_position_set(const square *sq, int _x, int _y);
+void square_position_set(square *sq, int _x, int _y);
 
-void square_size_set(const square *sq, int _width, int _height);
+void square_size_set(square *sq, int _width, int _height);
 
-void square_free(const square *sq);
+void square_free(square *sq);
 
 #endif
