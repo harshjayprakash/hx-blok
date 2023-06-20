7c741fb2a7f7cae8c92cc18b71bd6b69ce2a58ae
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-20 13:47:21 +0100

    Created function stubs.

--- src/model/square.c
+++ src/model/square.c
@@ -1,1 +1,21 @@
 #include "square.h"
+
+square square_new(int _x, int _y, int _width, int _height)
+{
+
+}
+
+void square_position_set(square *sq, int _x, int _y)
+{
+
+}
+
+void square_size_set(square *sq, int _width, int _height)
+{
+
+}
+
+void square_free(square *sq)
+{
+    
+}
