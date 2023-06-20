4342f8b3b16b64a86b0acec795d2efad0b02bc3d
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-20 13:20:48 +0100

    Added static instance check.

--- src/core/program.c
+++ src/core/program.c
@@ -1,17 +1,23 @@
 #include "program.h"
 
 static program s_program_instance;
+static int s_initialised = 0;
+
 void program_init(
     HINSTANCE _instance_handle, 
     HINSTANCE _previous_instance_handle, 
     LPSTR _command_line_arguments, 
     int _show_flag)
 {
+    if (s_initialised)
+        return;
+    
     s_program_instance.instance_handle = _instance_handle;
     s_program_instance.previous_instance_handle = _previous_instance_handle;
     s_program_instance.command_line_arguments = _command_line_arguments;
     s_program_instance.show_flag = _show_flag;
 
+    s_initialised = 1;
 }
 
 program *program_instance_get(void)
