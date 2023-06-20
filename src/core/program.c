3ec076b6f9539a4c155d825ada96c2c13455e810
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-20 13:21:06 +0100

    Added doxygen comments.

--- src/core/program.c
+++ src/core/program.c
@@ -1,6 +1,13 @@
 #include "program.h"
 
+/**
+ * @brief Stores an instance of the program structure.
+ */
 static program s_program_instance;
+
+/**
+ * @brief Stores whether the program structure has been initialised.
+ */
 static int s_initialised = 0;
 
 void program_init(
