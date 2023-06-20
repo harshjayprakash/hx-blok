8217727d8b90181aab8d2fca9bf062849fac786a
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-20 13:19:50 +0100

    Added function implementation.

--- src/core/program.c
+++ src/core/program.c
@@ -7,11 +7,16 @@ void program_init(
     LPSTR _command_line_arguments, 
     int _show_flag)
 {
+    s_program_instance.instance_handle = _instance_handle;
+    s_program_instance.previous_instance_handle = _previous_instance_handle;
+    s_program_instance.command_line_arguments = _command_line_arguments;
+    s_program_instance.show_flag = _show_flag;
 
 }
 
 program *program_instance_get(void)
 {
+    return &s_program_instance;
 }
 
 void program_free(void)
