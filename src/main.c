1329d6e5f66de0e7943492cf50d3e26147198d76
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-21 13:47:16 +0100

    Added window.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include <windows.h>
 
 #include "core/program.h"
+#include "presentation/window.h"
 
 /**
  * @brief Provides a entrypoint into the program.
@@ -18,6 +20,13 @@ int APIENTRY WinMain(
         int show_flag) {
     
     program_init(instance_handle, previous_instance_handle, command_line_arguments, show_flag);
+
+    optional_window window_ = window_new();
+
+    if (!window_.is_present)
+        fprintf(stderr, "Failed to initialise window");
+    
+    window_free(&window_.get.instance);
     program_free();
 
     return 0;
