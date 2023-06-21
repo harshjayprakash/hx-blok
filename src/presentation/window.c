aa082992b294c51033091d78650fe1fef7d5d54c
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-21 13:40:48 +0100

    Added additional static functions.

--- src/presentation/window.c
+++ src/presentation/window.c
@@ -1,5 +1,32 @@
 #include "window.h"
 
+static int window_action_while_running(HWND window_handle, WPARAM word_param, LPARAM long_param)
+{
+}
+static int window_action_on_close(HWND window_handle, WPARAM word_param, LPARAM long_param)
+{
+}
+
+static long long window_callback(
+    HWND window_handle, 
+    UINT message, 
+    WPARAM word_param, 
+    LPARAM long_param)
+{
+}
+
+static int window_message_loop(window *wnd)
+{
+}
+
+static ATOM window_register(window *wnd)
+{
+}
+
+static int window_create(window *wnd)
+{
+}
+
 optional_window window_new(void)
 {
 }
