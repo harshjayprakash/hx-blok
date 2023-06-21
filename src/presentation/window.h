ad68fcb227dee208f787fd69bd4077e3af9a27d3
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-21 13:36:31 +0100

    Created window structure.

--- src/presentation/window.h
+++ src/presentation/window.h
@@ -1,5 +1,20 @@
 #ifndef __BLOK_WINDOW_H
 #define __BLOK_WINDOW_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+#include <windows.h>
+
 #define CHAR_LENGTH_COUNT 260
+
+typedef struct window__
+{
+    HWND window_handle;
+    MSG window_message;
+    WNDCLASSEXW window_class;
+    wchar_t window_class_name[CHAR_LENGTH_COUNT];
+} window;
+
 #endif
