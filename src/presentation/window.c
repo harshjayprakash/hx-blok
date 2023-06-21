3966cf4dddff8adab7d349f34164eb4e9a78be01
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-21 13:42:21 +0100

    Implementated window registration.

--- src/presentation/window.c
+++ src/presentation/window.c
@@ -21,6 +21,25 @@ static int window_message_loop(window *wnd)
 
 static ATOM window_register(window *wnd)
 {
+    if (wnd == NULL)
+        return -1;
+    
+    (void) wcsncpy(wnd->window_class_name, L"blok_window_class", CHAR_LENGTH_COUNT);
+
+    wnd->window_class.cbSize = sizeof(WNDCLASSEXW);
+    wnd->window_class.style = CS_HREDRAW | CS_VREDRAW;
+    wnd->window_class.lpfnWndProc = window_callback;
+    wnd->window_class.cbClsExtra = 0;
+    wnd->window_class.cbWndExtra = 0;
+    wnd->window_class.hInstance = program_instance_get()->instance_handle;
+    wnd->window_class.hIcon = LoadIcon(wnd->window_class.hInstance, IDI_APPLICATION);
+    wnd->window_class.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wnd->window_class.hbrBackground = CreateSolidBrush(RGB(0, 0, 0));
+    wnd->window_class.lpszMenuName = NULL;
+    wnd->window_class.lpszClassName = wnd->window_class_name;
+    wnd->window_class.hIconSm = LoadIcon(wnd->window_class.hInstance, IDI_APPLICATION);
+
+    return RegisterClassExW(&wnd->window_class);
 }
 
 static int window_create(window *wnd)
@@ -29,6 +48,15 @@ static int window_create(window *wnd)
 
 optional_window window_new(void)
 {
+    optional_window opt_wnd;
+    window wnd;
+
+    opt_wnd.registered = window_register(&wnd);
+    if (opt_wnd.registered == 0)
+    {
+        window_free(&wnd);
+        return opt_wnd;
+    }
 }
 
 void window_free(window *wnd)
