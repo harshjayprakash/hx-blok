bc094f62f4a2a5f0d8d806c27665aa396584fd08
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-24 14:36:33 +0100

    Code reformatted.

--- src/core/program.c
+++ src/core/program.c
@@ -4,15 +4,12 @@ static struct program s_program_instance;
 
 static int s_initialised = 0;
 
-void program_init(
-    HINSTANCE _instance_handle, 
-    HINSTANCE _previous_instance_handle, 
-    LPSTR _command_line_arguments, 
-    int _show_flag)
-{
+void program_init(HINSTANCE _instance_handle,
+                  HINSTANCE _previous_instance_handle,
+                  LPSTR _command_line_arguments, int _show_flag) {
     if (s_initialised)
         return;
-    
+
     s_program_instance.instance_handle = _instance_handle;
     s_program_instance.previous_instance_handle = _previous_instance_handle;
     s_program_instance.command_line_arguments = _command_line_arguments;
@@ -21,12 +18,6 @@ void program_init(
     s_initialised = 1;
 }
 
-program *program_instance_get(void)
-{
-    return &s_program_instance;
-}
+struct program *program_instance_get(void) { return &s_program_instance; }
 
-void program_free(void)
-{
-
-}
+void program_free(void) {}
