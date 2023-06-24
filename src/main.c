c6f5e729cc7257d054e850b67bfdd81b7e532c7f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-24 14:19:22 +0100

    Reformatted.

--- src/main.c
+++ src/main.c
@@ -13,19 +13,18 @@
  * @return An integer indicating whether the program operation was successful.
  *      zero for success, anything else for failure.
  */
-int APIENTRY WinMain(
-        HINSTANCE instance_handle, 
-        HINSTANCE previous_instance_handle, 
-        LPSTR command_line_arguments, 
-        int show_flag) {
-    
-    program_init(instance_handle, previous_instance_handle, command_line_arguments, show_flag);
+int APIENTRY WinMain(HINSTANCE instance_handle,
+                     HINSTANCE previous_instance_handle,
+                     LPSTR command_line_arguments, int show_flag) {
+
+    program_init(instance_handle, previous_instance_handle,
+                 command_line_arguments, show_flag);
 
     optional_window window_ = window_new();
 
     if (!window_.is_present)
         fprintf(stderr, "Failed to initialise window");
-    
+
     window_free(&window_.get.instance);
     program_free();
 
