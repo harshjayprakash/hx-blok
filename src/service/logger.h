f920f20a3c3cf9e3ecf26f65af02e2441d39f8eb
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-25 14:25:46 +0100

    Added `printlog` function.

--- src/service/logger.h
+++ src/service/logger.h
@@ -1,11 +1,15 @@
 #ifndef BLOK_LOGGER_H
 #define BLOK_LOGGER_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
 enum log_level {
     log_info,
     log_warn,
     log_err,
 };
+
+void printlog(enum log_level _level, const char *_where, const char *_message);
 
 #endif
