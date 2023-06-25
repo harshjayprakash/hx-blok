24fe4c8dbdf3f662315b0c74b5763a5e7b617bb6
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-25 14:26:13 +0100

    Added global getters.

--- src/service/store.h
+++ src/service/store.h
@@ -1,4 +1,12 @@
 #ifndef BLOK_STORE_H
 #define BLOK_STORE_H
 
+#include <windows.h>
+
+#include "../model/square.h"
+
+struct square *global_square_get(void);
+
+RECT global_square_get_winapi_rect();
+
 #endif
