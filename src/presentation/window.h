3c64d810a32f99503b2830e8ef39ef5eac19c04b
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-25 14:04:08 +0100

    Added doxygen comments.

--- src/presentation/window.h
+++ src/presentation/window.h
@@ -12,25 +12,70 @@
 
 #define CHAR_LENGTH_COUNT 260
 
+/**
+ * @brief Contains all the components for the creation of a window.
+ */
 struct window {
+    /**
+     * @brief Stores the handle to the window.
+     */
     HWND window_handle;
+
+    /**
+     * @brief Stores the message or events that occur when the window is
+     * running.
+     */
     MSG window_message;
+
+    /**
+     * @brief Stores the native window class information.
+     */
     WNDCLASSEXW window_class;
+
+    /**
+     * @brief Stores the name of the native window class.
+     */
     wchar_t window_class_name[CHAR_LENGTH_COUNT];
 };
 
+/**
+ * @brief Stores the potential instance of the window or null.
+ */
 union window_instance_handler {
+    /**
+     * @brief Stores the instance of the window.
+     */
     struct window instance;
+    /**
+     * @brief Stores NULL.
+     */
     void *potential_instance;
 };
 
+/**
+ * @brief Provides a wrapper around the window structure with varies checks to
+ * ensure that window creation was successful.
+ */
 struct window_wrapper {
+    /**
+     * @brief Stores whether window registration was successful.
+     */
     bool registered;
 
+    /**
+     * @brief Stores whether the window has been created.
+     */
     bool created;
 
+    /**
+     * @brief Stores whether the instance of the window is stored in this
+     * wrapper.
+     */
     bool present;
 
+    /**
+     * @brief Stores the potential instance of the window or null.
+     */
     union window_instance_handler handle;
 };
 
