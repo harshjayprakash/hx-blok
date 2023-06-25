a80e2da9f20e0dad40f333052439648f8ed158f0
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-25 14:26:22 +0100

    Added implementation.

--- src/service/store.c
+++ src/service/store.c
@@ -1,1 +1,12 @@
 #include "store.h"
+
+struct square *global_square_get(void) {
+    static struct square sq;
+    return &sq;
+}
+
+RECT global_square_get_winapi_rect() {
+    struct square *sq = global_square_get();
+    RECT rect = { sq->x, sq->y, sq->x + sq->width, sq->y + sq->height };
+    return rect;
+}
