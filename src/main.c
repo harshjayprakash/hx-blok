546c5b32c85a7c433ba798a760821e1a7b253e27
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-28 13:03:22 +0100

    `clang-tidy`: Updated parameter names.

--- src/main.c
+++ src/main.c
@@ -9,19 +9,17 @@
 
 /**
  * @brief Provides a entrypoint into the program.
- * @param instance_handle program instance handle.
- * @param previous_instance_handle previous version of the instance handle.
- * @param command_line_arguments command line arguments specified.
- * @param show_flag how the application will be shown.
+ * @param hInstance program instance handle.
+ * @param hPrevInstance previous version of the instance handle.
+ * @param lpCmdLine command line arguments specified.
+ * @param nShowCmd how the application will be shown.
  * @return An integer indicating whether the program operation was successful.
  *      zero for success, anything else for failure.
  */
-int APIENTRY WinMain(HINSTANCE instance_handle,
-                     HINSTANCE previous_instance_handle,
-                     LPSTR command_line_arguments, int show_flag) {
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                     LPSTR lpCmdLine, int nShowCmd) {
 
-    program_init(instance_handle, previous_instance_handle,
-                 command_line_arguments, show_flag);
+    program_init(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
 
     global_square_get()->y = 0;
     global_square_get()->x = 0;
