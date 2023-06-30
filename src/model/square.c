bc47f1e10114821555e25c62dec72d7b8fc41795
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-06-30 20:42:38 +0100

    Added null checks.

--- src/model/square.c
+++ src/model/square.c
@@ -6,13 +6,19 @@ struct square square_new(int _x, int _y, int _width, int _height) {
 }
 
 void square_position_set(struct square *sq, int _x, int _y) {
+    if (!sq)
+        return;
+
     sq->x = _x;
     sq->y = _y;
 }
 
 void square_size_set(struct square *sq, int _width, int _height) {
+    if (!sq)
+        return;
+
     sq->width = _width;
     sq->height = _height;
 }
 
-void square_free(struct square *sq) {}
+void square_free(struct square *sq) { }
