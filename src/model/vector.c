f3906b5f1ba8f1d843742fae11a6ce27720842dc
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-05 15:32:00 +0100

    Implementated.

--- src/model/vector.c
+++ src/model/vector.c
@@ -13,21 +13,15 @@ int vector_resize(struct vector *vec, int _new_size)
 {
     if (!vec)
         return BLOK_VECTOR_NULL;
-
     if (!vec->dyn_array)
         return BLOK_VECTOR_ARRAY_NULL;
-
     if (_new_size < vec->max + 1)
         return BLOK_VECTOR_INVALID_SIZE;
-
     struct mark *new_mem = realloc(vec->dyn_array, _new_size * sizeof(struct mark));
-
     if (!new_mem)
         return BLOK_VECTOR_REALLOC_FAILED;
-
     vec->dyn_array = new_mem;
     vec->max = _new_size;
-
     return BLOK_OPERATION_SUCCESS;
 }
 
@@ -35,7 +29,6 @@ int vector_push(struct vector *vec, struct mark _node)
 {
     if (vec->head == vec->max)
         return BLOK_VECTOR_CAPACITY_FULL;
-
     vec->dyn_array[vec->head++] = _node;
     return BLOK_OPERATION_SUCCESS;
 }
@@ -46,7 +39,6 @@ struct mark *vector_get_ptr(const struct vector *vec, int _index)
     {
         return NULL;
     }
-
     return (vec->dyn_array + (_index - 1));
 }
 
@@ -55,11 +47,9 @@ struct mark vector_get(const struct vector *vec, int _index)
     struct mark value = {0, 0, mark_invisible};
     if (!vec)
         return value;
-
     struct mark *result = vector_get_ptr(vec, _index);
     if (!result)
         return value;
-
     return *result;
 }
 
@@ -67,7 +57,6 @@ int vector_next_available_index(const struct vector *vec)
 {
     if (!vec)
         return BLOK_NULL_POINTER_ERROR;
-
     for (int i = 0; i < vec->head + 1; i++)
     {
         if (vec->dyn_array[i].illumated == mark_invisible)
@@ -75,18 +64,51 @@ int vector_next_available_index(const struct vector *vec)
             return i;
         }
     }
-
     if (vec->head + 1 > vec->max)
         return BLOK_VECTOR_CAPACITY_FULL;
-
     return vec->head + 1;
+}
+
+struct mark vector_pop(struct vector *vec)
+{
+    struct mark mark_ = {0, 0, mark_invisible};
+    if (!vec)
+        return mark_;
+    if (!vec->dyn_array)
+        return mark_;
+    mark_ = vec->dyn_array[vec->head];
+    vec->dyn_array[vec->head].illumated = mark_invisible;
+    vec->head--;
+    return mark_;
+}
+
+struct mark vector_peak(const struct vector *vec)
+{
+    struct mark mark_ = {0, 0, mark_invisible};
+    if (!vec)
+        return mark_;
+    return vec->dyn_array[vec->head];
+}
+
+struct mark vector_remove_index(struct vector *vec, int _index)
+{
+    struct mark mark_ = {0, 0, mark_invisible};
+    if (!vec)
+        return mark_;
+    if (vector_get_ptr(vec, _index) == NULL)
+        return mark_;
+    
+}
+
+int vector_remove_object(struct vector *vec, struct mark _node)
+{
+
 }
 
 void vector_free(struct vector *vec)
 {
     if (!vec)
         return;
-
     if (vec->dyn_array)
         free(vec->dyn_array);
 }
