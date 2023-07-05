d191c7f4730e5877ec9a7fcb01f70a99c318d873
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-05 12:54:44 +0100

    Formatted with Clang-Format.

--- src/core/program.c
+++ src/core/program.c
@@ -4,20 +4,23 @@ static struct program s_program_instance;
 
 static int s_initialised = 0;
 
-void program_init(HINSTANCE _instance_handle,
-                  HINSTANCE _previous_instance_handle,
-                  LPSTR _command_line_arguments, int _show_flag) {
+void program_init(HINSTANCE _instance_handle, HINSTANCE _previous_instance_handle, LPSTR _command_line_arguments,
+                  int _show_flag)
+{
     if (s_initialised)
         return;
-
     s_program_instance.instance_handle = _instance_handle;
     s_program_instance.previous_instance_handle = _previous_instance_handle;
     s_program_instance.command_line_arguments = _command_line_arguments;
     s_program_instance.show_flag = _show_flag;
-
     s_initialised = 1;
 }
 
-struct program *program_instance_get(void) { return &s_program_instance; }
+struct program *program_instance_get(void)
+{
+    return &s_program_instance;
+}
 
-void program_free(void) {}
+void program_free(void)
+{
+}
