f2e6591afaea690cab5c2b63b4ea0b4047b82ac3
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-07 21:06:35 +0100

    Removed.

--- src/model/mark.c
+++
@@ -1,7 +0,0 @@
-#include "mark.h"
-
-struct mark mark_new(int x, int y)
-{
-    struct mark mark_ = {x, y, mark_visible};
-    return mark_;
-}
--- src/service/store.c
+++
@@ -1,7 +0,0 @@
-#include "store.h"
-
-struct store *store_instance_get(void)
-{
-    static struct store store_;
-    return &store_;
-}
