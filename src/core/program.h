f473b9f21af72dc51ffaa7c40cd81e51d677f9f6
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:25:56 +0100

    Created `Program` structure.

--- src/core/program.h
+++ src/core/program.h
@@ -1,4 +1,14 @@
 #ifndef BLOK_PROGRAM_H
 #define BLOK_PROGRAM_H
 
+#include <Windows.h>
+
+struct Program
+{
+    HINSTANCE instanceHandle;
+    HINSTANCE previousInstanceHandle;
+    LPSTR commandLineArguments;
+    int showFlag;
+};
+
 #endif
