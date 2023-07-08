f644f0297dc584fc480c0c397ab0304cf71ed0de
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:38:17 +0100

    Implementated mark associated functions.

--- src/model/mark.c
+++ src/model/mark.c
@@ -1,2 +1,14 @@
 #include "mark.h"
 
+struct Mark blokMarkNew(int _positionX, int _positionY)
+{
+    return (struct Mark){ _positionX, _positionY };
+}
+
+void blokMarkPositionSet(struct Mark *mark, int _positionX, int _positionY)
+{
+    BLOK_EXIT_IF(!mark);
+
+    mark->positionX = _positionX;
+    mark->positionY = _positionY;
+}
