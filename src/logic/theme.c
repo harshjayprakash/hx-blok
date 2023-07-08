bd1b6a1f0615549fdf68a67d61e7a2d234c8207b
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:41:24 +0100

    Implementated theme getter and setters.

--- src/logic/theme.c
+++ src/logic/theme.c
@@ -1,2 +1,13 @@
 #include "theme.h"
 
+static enum Theme theme = ThemeLight;
+
+void blokThemeSet(enum Theme _theme)
+{
+    theme = _theme;
+}
+
+enum Theme blokThemeGet(void)
+{
+    return theme;
+}
