3fbf0b16b8f1be52dbea7747ce9f1971c4c10532
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:41:40 +0100

    Implementated colour switches.

--- src/logic/theme.c
+++ src/logic/theme.c
@@ -1,6 +1,10 @@
 #include "theme.h"
 
 static enum Theme theme = ThemeLight;
+static unsigned long colourBlack = RGB(0, 0, 0);
+static unsigned long colourWhite = RGB(255, 255, 255);
+static unsigned long colourBlue = RGB(0, 15, 254);
+static unsigned long colourPink = RGB(255, 32, 100);
 
 void blokThemeSet(enum Theme _theme)
 {
@@ -10,4 +14,40 @@ void blokThemeSet(enum Theme _theme)
 enum Theme blokThemeGet(void)
 {
     return theme;
+}
+
+unsigned long blokColourBackgroundGet(void)
+{
+    switch (theme)
+    {
+    case ThemeDark:
+        return colourBlack;
+    case ThemeLight:
+    default:
+        return colourWhite;
+    }
+}
+
+unsigned long blokColourForegroundGet(void)
+{
+    switch (theme)
+    {
+    case ThemeDark:
+        return colourWhite;
+    case ThemeLight:
+    default:
+        return colourBlack;
+    }
+}
+
+unsigned long blokColourMarkGet(void)
+{
+    switch (theme)
+    {
+    case ThemeDark:
+        return colourBlue;
+    case ThemeLight:
+    default:
+        return colourPink;
+    }
 }
