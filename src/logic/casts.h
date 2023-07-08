3be590ebe4f12087c6219de7244e1de8d5b535b3
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:34:58 +0100

    Added winapi rect cast.

--- src/logic/casts.h
+++ src/logic/casts.h
@@ -1,6 +1,11 @@
 #ifndef BLOK_CASTS_H
 #define BLOK_CASTS_H
 
+#include <Windows.h>
 
+#include "../core/macros.h"
+#include "../model/square.h"
+
+RECT blokCastSquareToWinApiRect(struct Square *square);
 
 #endif
