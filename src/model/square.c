c0f3c5005838dafd568fb8122d2fbac34386c9ca
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:31:44 +0100

    Implemented functions.

--- src/model/square.c
+++ src/model/square.c
@@ -1,2 +1,23 @@
 #include "square.h"
 
+struct Square blokSquareNew(int _positionX, int _positionY, int _width, int _height)
+{
+    return (struct Square){ _positionX, _positionY, _width, _height };
+}
+
+void blokSquarePositionSet(struct Square *square, int _positionX, int _positionY)
+{
+    BLOK_EXIT_IF(!square);
+
+    square->positionX = _positionX;
+    square->positionY = _positionY;
+}
+
+void blokSquareSizeSet(struct Square *square, int _width, int _height)
+{
+    BLOK_EXIT_IF(!square);
+
+    square->width = _width;
+    square->height = _height;
+}
+
