72bade348d0d0d19d431c2bffac0e79704c9a566
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:56:07 +0100

    Added debug logging functionality.

--- src/core/debug.c
+++ src/core/debug.c
@@ -1,1 +1,32 @@
 #include "debug.h"
+
+static FILE *consoleIn;
+static FILE *consoleOut;
+static FILE *consoleErr;
+
+void blokDebugConsoleOpen()
+{
+    consoleIn = stdin;
+    consoleOut = stdout;
+    consoleErr = stderr;
+    AllocConsole();
+    AttachConsole(GetCurrentProcessId());
+    freopen_s(&consoleIn, "CONIN$", "r", stdin);
+    freopen_s(&consoleOut, "CONOUT$", "w", stdout);
+    freopen_s(&consoleErr, "CONOUT$", "w", stderr);
+    SetConsoleTitleW(L"The Blok Experiment Console");
+    printf("======= THE BLOCK EXPERIMENT DEBUGGING MECHANISM =======\n");
+}
+
+void blokDebugConsoleClose()
+{
+    fclose(consoleIn);
+    fclose(consoleOut);
+    fclose(consoleErr);
+    FreeConsole();
+}
+
+void blokDebugConsoleLog(const char *message)
+{
+    printf("%s\n", message);
+}
--- src/core/debug.h
+++ src/core/debug.h
@@ -1,5 +1,36 @@
 #ifndef BLOK_DEBUG_H
 #define BLOK_DEBUG_H
 
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+
+#define BLOK_DEBUG_MECHANISMS_ENABLED 1
+
+void blokDebugConsoleOpen();
+
+void blokDebugConsoleClose();
+
+void blokDebugConsoleLog(const char *message);
+
+#if BLOK_DEBUG_MECHANISMS_ENABLED
+
+#define BLOK_DEBUG_CONSOLE_OPEN() blokDebugConsoleOpen()
+
+#define BLOK_DEBUG_CONSOLE_CLOSE() blokDebugConsoleClose()
+
+#define BLOK_LOG(MESSAGE) blokDebugConsoleLog(MESSAGE)
+
+#else
+
+#define BLOK_DEBUG_CONSOLE_OPEN() 
+
+#define BLOK_DEBUG_CONSOLE_CLOSE() 
+
+#define BLOK_LOG(MESSAGE)
+
+#endif
 
 #endif
