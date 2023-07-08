9612cb4f4d827febcd0a1cdf73f2664ba1944a65
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:49:42 +0100

    Added window calls.

--- src/main.c
+++ src/main.c
@@ -5,5 +5,7 @@ int APIENTRY WinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
     blokProgramInitialise(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+    struct WindowWrapper windowWrapper = blokWindowNew();
+    blokWindowFree(&windowWrapper.optionalInstance.actualInstance);
     blokProgramFree();
 }
