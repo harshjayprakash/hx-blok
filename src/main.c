5ee9ee0166ab468f223931ea4ef73f12e7eecfcd
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:56:25 +0100

    Added debug initialiser calls.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include <Windows.h>
+
 #include "core/program.h"
+#include "core/debug.h"
 #include "presentation/window.h"
 #include "logic/store.h"
 
 int APIENTRY WinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+    BLOK_DEBUG_CONSOLE_OPEN();
+
     blokProgramInitialise(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
     blokStoreInitialise();
     struct WindowWrapper windowWrapper = blokWindowNew();
@@ -13,4 +17,6 @@ int APIENTRY WinMain(
     blokWindowFree(&windowWrapper.optionalInstance.actualInstance);
     blokStoreFree();
     blokProgramFree();
+    
+    BLOK_DEBUG_CONSOLE_CLOSE();
 }
