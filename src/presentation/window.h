6b7b85bbd7472e394f385a7757bdeb73ef92732e
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:39:28 +0100

    Added includes.

--- src/presentation/window.h
+++ src/presentation/window.h
@@ -1,5 +1,17 @@
 #ifndef BLOK_WINDOW_H
 #define BLOK_WINDOW_H
 
+#include <Windows.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <wchar.h>
+
+#include "../core/macros.h"
+#include "../core/program.h"
+#include "../core/debug.h"
+#include "../logic/theme.h"
+#include "../logic/store.h"
+#include "../logic/casts.h"
 
 #endif
