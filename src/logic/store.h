34f6ee4d482aa967667107f7b87854c30066ffef
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:51:52 +0100

    Created `Store` structure.

--- src/logic/store.h
+++ src/logic/store.h
@@ -1,5 +1,14 @@
 #ifndef BLOK_STORE_H
 #define BLOK_STORE_H
 
+#include "../model/square.h"
+#include "../model/vector.h"
+
+struct Store
+{
+    struct Square movableSquare;
+    struct Vector markedRegions;
+};
+
 
 #endif
