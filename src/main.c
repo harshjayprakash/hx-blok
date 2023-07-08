72cfc00277042e20c6930368c20aa4d41740b3b8
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:52:52 +0100

    Added store init calls.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <Windows.h>
 #include "core/program.h"
 #include "presentation/window.h"
+#include "logic/store.h"
 
 int APIENTRY WinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
     blokProgramInitialise(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+    blokStoreInitialise();
     struct WindowWrapper windowWrapper = blokWindowNew();
+
     blokWindowFree(&windowWrapper.optionalInstance.actualInstance);
+    blokStoreFree();
     blokProgramFree();
 }
