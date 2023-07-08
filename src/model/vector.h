45964843d36346d5a869001ccad82dc01052b644
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:48:19 +0100

    Created `Vector` structure.

--- src/model/vector.h
+++ src/model/vector.h
@@ -1,5 +1,17 @@
 #ifndef BLOK_VECTOR_H
 #define BLOK_VECTOR_H
 
+#include <stdlib.h>
+#include <memory.h>
+
+#include "mark.h"
+#include "../core/macros.h"
+
+struct Vector
+{
+    struct Mark *arr;
+    int max;
+    int head;
+};
 
 #endif
