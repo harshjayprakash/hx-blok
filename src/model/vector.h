39ff75d25bbcc3e893144b09c7ce52f7bb41574b
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:48:33 +0100

    Added associated functions.

--- src/model/vector.h
+++ src/model/vector.h
@@ -14,4 +14,34 @@ struct Vector
     int head;
 };
 
+struct Vector blokVectorNew(int _size);
+
+int blokVectorGenerateNewSize(int _size);
+
+int blokVectorResize(struct Vector *vector, int _newSize);
+
+int blokVectorFull(struct Vector *vector);
+
+int blokVectorEmpty(struct Vector *vector);
+
+int blokVectorPush(struct Vector *vector, struct Mark _node);
+
+struct Mark *blokVectorPointerGet(struct Vector *vector, int _index);
+
+struct Mark blokVectorGet(struct Vector *vector, int _index);
+
+struct Mark blokVectorPop(struct Vector *vector);
+
+struct Mark blokVectorPeak(struct Vector *vector);
+
+int blokVectorRemoveIndex(struct Vector *vector, int _index);
+
+int blokVectorRemoveObject(struct Vector *vector, struct Mark _node);
+
+int blokVectorFind(struct Vector *vector, struct Mark _node);
+
+int blokVectorNextAvailableIndex(struct Vector *vector);
+
+void blokVectorFree(struct Vector *vector);
+
 #endif
