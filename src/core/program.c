91cf8f887e292f5c4aedb0773de6b13fc8875603
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:27:45 +0100

    Implementated functions.

--- src/core/program.c
+++ src/core/program.c
@@ -3,3 +3,32 @@
 static struct Program singletonInstance = { };
 static int initialised = 0;
 
+static void blokProgramProcessArguments()
+{
+}
+
+void blokProgramInitialise(
+    HINSTANCE _instanceHandle, HINSTANCE _previousInstanceHandle, 
+    LPSTR _commandLineArguments, int _showFlag)
+{
+    BLOK_EXIT_IF(initialised);
+
+    singletonInstance.instanceHandle = _instanceHandle;
+    singletonInstance.previousInstanceHandle = _previousInstanceHandle;
+    singletonInstance.commandLineArguments = _commandLineArguments;
+    singletonInstance.showFlag = _showFlag;
+
+    blokProgramProcessArguments();
+
+    initialised = 1;
+}
+
+struct Program *blokProgramInstanceGet(void)
+{
+    return &singletonInstance;
+}
+
+void blokProgramFree(void)
+{
+    
+}
