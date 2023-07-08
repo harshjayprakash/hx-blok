2eee6c534277a32b21cca95c19e52e3de8cb4955
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-08 19:35:20 +0100

    Implemented cast function.

--- src/logic/casts.c
+++ src/logic/casts.c
@@ -1,2 +1,10 @@
 #include "casts.h"
 
+RECT blokCastSquareToWinApiRect(struct Square *square)
+{
+    BLOK_NON_VOID_EXIT_IF(!square, ((RECT){ 0, 0, 15, 15 }));
+
+    return (RECT) { 
+        square->positionX, square->positionY, 
+        (square->positionX + square->width), (square->positionY + square->height) };
+}
