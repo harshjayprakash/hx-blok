b94c739abbf4d3f251a73c76804472ce141939e4
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-10 14:22:46 +0100

    Added doxygen documentation.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,28 @@
 #include "presentation/window.h"
 #include "logic/store.h"
 
+/**
+ * @brief Entrypoint for the program.
+ * 
+ * Responsible for starting up the program, initialising any globally stored data and 
+ * creation of the window. After this, cleaning up resources when terminating.
+ * 
+ * @param hInstance The handle to the current instance of the program.
+ * @param hPrevInstance Always NULL in this program.
+ * @param lpCmdLine A pointer to a null terminated string that specifies the command line
+ *                  for the program, excluding the program name.
+ * @param nCmdShow Controls how the window is to be shown.
+ * 
+ * @details The following tasks are executed:
+ *          - Starts a console to view debugging information
+ *          - Initialises the program handles store
+ *          - Initialises the program's standard globals store.
+ *          - Creates, initialises and runs the main window.
+ *          - Then cleans up any resources used when the window is terminated.
+ * 
+ * @note This is the standard ASCII WinMain entrypoint due to Mingw GCC not supporting
+ *       the unicode version of wWinMain.
+ */
 int APIENTRY WinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
