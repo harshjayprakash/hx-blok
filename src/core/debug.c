4517e49991d23e607b3b7b25ba040e32038676db
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-19 14:04:32 +0100

    Removed additional streams. Casted to void.

--- src/core/debug.c
+++ src/core/debug.c
@@ -14,40 +14,24 @@
 #include "debug.h"
 
 /**
- * @brief Input stream for the console.
- */
-static FILE *consoleIn;
-
-/**
  * @brief Output stream for the console.
  */
 static FILE *consoleOut;
 
-/**
- * @brief Error stream for the console.
- */
-static FILE *consoleErr;
-
 void blokDebugConsoleOpen()
 {
-    consoleIn = stdin;
     consoleOut = stdout;
-    consoleErr = stderr;
-    AllocConsole();
-    AttachConsole(GetCurrentProcessId());
-    freopen_s(&consoleIn, "CONIN$", "r", stdin);
-    freopen_s(&consoleOut, "CONOUT$", "w", stdout);
-    freopen_s(&consoleErr, "CONOUT$", "w", stderr);
-    SetConsoleTitleW(L"The Blok Experiment Console");
-    printf("======= THE BLOCK EXPERIMENT DEBUGGING MECHANISM =======\n");
+    (void)AllocConsole();
+    (void)AttachConsole(GetCurrentProcessId());
+    (void)freopen_s(&consoleOut, "CONOUT$", "w", stdout);
+    (void)SetConsoleTitleW(L"The Blok Experiment Console");
+    (void)printf("======= THE BLOCK EXPERIMENT DEBUGGING MECHANISM =======\n");
 }
 
 void blokDebugConsoleClose()
 {
-    fclose(consoleIn);
-    fclose(consoleOut);
-    fclose(consoleErr);
-    FreeConsole();
+    (void)fclose(consoleOut);
+    (void)FreeConsole();
 }
 
 void blokDebugConsoleLog(const char *message)
