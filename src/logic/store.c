f9768feb31cf624ec4b10131b4646ceb93d0e391
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-19 14:20:52 +0100

    Created doxygen documentation.

--- src/logic/store.c
+++ src/logic/store.c
@@ -1,5 +1,22 @@
+/**
+ * @file store.c
+ * @version 0.6.1
+ * @date 18-07-2023
+ * 
+ * @brief Global store management.
+ * 
+ * This file contains the functions for the global store instance management as defined
+ * in corresponding to the header file.
+ * 
+ * @implements store.h
+ */
+
 #include "store.h"
 
+/**
+ * @brief Stores the internal instance of the store, acessable externally through the
+ *        blokStoreInstanceGet() function.
+ */
 static struct Store instance;
 
 void blokStoreInitialise(void)
