80ed043db920ba9205862811a35bd1826490e189
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-07-19 17:16:10 +0100

    Created doxygen documentation.

--- src/model/compass.h
+++ src/model/compass.h
@@ -1,11 +1,40 @@
+/**
+ * @file compass.h
+ * @version 0.6.1
+ * @date 18-07-2023
+ * 
+ * @brief Directional representation.
+ * 
+ * This header file contains an enumeration for representing a direction via the compass
+ * rose.
+ */
+
 #ifndef BLOK_COMPASS_H
 #define BLOK_COMPASS_H
 
+/**
+ * @brief Describes the direction with compass terms.
+ */
 enum CompassRose
 {
+    /**
+     * @brief Represents the up movement (north).
+     */
     DirectionNorth,
+
+    /**
+     * @brief Represents the right movement (east).
+     */
     DirectionEast,
+
+    /**
+     * @brief Represents the down movement (south).
+     */
     DirectionSouth,
+
+    /**
+     * @brief Represents the left movement (wes).
+     */
     DirectionWest
 };
 
