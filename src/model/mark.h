34ffa542d3fdea0f0e73a88cd411f8fb6a385a76
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-09-14 08:41:31 +0100

    Added io header.

--- src/model/mark.h
+++ src/model/mark.h
@@ -12,6 +12,8 @@
 #ifndef BLOK_MARK_H
 #define BLOK_MARK_H
 
+#include <stdio.h>
+
 #include "../core/macros.h"
 
 /**
@@ -48,5 +50,32 @@ struct Mark blokMarkNew(int _positionX, int _positionY);
  * @param _positionY The new y position.
  */
 void blokMarkPositionSet(struct Mark *mark, int _positionX, int _positionY);
+
+/**
+ * @brief Compares the marks passed as arguments.
+ * 
+ * @details This function individually compares all the values within the mark structure
+ *          with both arguments.
+ * 
+ * @param firstMark The first mark to be compared.
+ * @param secondMark The second mark to be compared.
+ * @return An integer indicating whether the objects values match or not.
+ * 
+ * @retval BLOK_OBJECT_EQUALS: The objects provided match.
+ * @retval BLOK_OBJECT_NOT_EQUALS: The objects provided do not match.
+ * 
+ * @see macro.h
+ */
+int blokMarkEquals(struct Mark firstMark, struct Mark secondMark);
+
+/**
+ * @brief Prints the mark's information to the console.
+ * 
+ * @details This function combines the given mark's information into a single string
+ *          printing it to the debug console.
+ * 
+ * @param mark the instance of the mark.
+ */
+void blokMarkPrint(struct Mark *mark);
 
 #endif
