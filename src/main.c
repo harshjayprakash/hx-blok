dcdfe9a946f2fe5b235668e119c7e69dbc400888
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-04-03 21:44:22 +0100

    Add WinMain

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#ifndef _UNICODE
+#define _UNICODE
+#endif
+
+#if _WIN32
+
+#include <Windows.h>
+#include "core/common/macros.h"
+#include "core/common/program.h"
+
+int __stdcall WinMain(
+    HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+    LPSTR lpCmdLine, int nShowCmd)
+{
+    bloqDiscard(hPrevInstance);
+    bloqDiscard(lpCmdLine);
+
+    bloqInitProgram();
+    bloqSaveArguments(hInstance, nShowCmd);
+    bloqProcessArguments();
+    bloqExitProgram();
+
+    return 0;
+}
+
+#else
+#error "This program can only be compiled on Windows."
+#endif
