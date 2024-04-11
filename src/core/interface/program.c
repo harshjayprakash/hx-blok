b6ab367f83576306eb60168194675d32ee7af2ce
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-04-11 13:01:32 +0100

    Implement program module.

---
+++ src/core/interface/program.c
@@ -0,0 +1,55 @@
+#include "program.h"
+#include "../../presentation/window/pane.h"
+#include <Windows.h>
+#include <stdio.h>
+
+static int sInitialised = 0;
+static HINSTANCE sInstanceHandle = { 0 };
+static int sShowFlag = 0;
+
+void bloqInitProgram(void)
+{
+    if (sInitialised) { return; }
+
+    sInitialised = 1;
+}
+
+void bloqSaveArguments(HINSTANCE instanceHandle, int showFlag)
+{
+    if (!sInitialised) { return; }
+
+    sInstanceHandle = instanceHandle;
+    sShowFlag = showFlag;
+}
+
+void bloqProcessArguments(void)
+{
+    if (!sInitialised) { return; }
+}
+
+void bloqStart(void)
+{
+    if (!sInitialised) { return; }
+
+    TPane pane = bloqNewPane();
+    bloqFreePane(&pane);
+}
+
+HINSTANCE bloqGetInstanceHandle(void)
+{
+    if (!sInitialised) { return NULL; }
+
+    return sInstanceHandle;
+}
+
+int bloqGetShowFlag(void)
+{
+    return sShowFlag;
+}
+
+void bloqExitProgram(void)
+{
+    if (!sInitialised) { return; }
+
+    sInitialised = 0;
+}
