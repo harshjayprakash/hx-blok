9fcff492579536c1974db970dfa3cd90a07da265
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-04-18 19:52:04 +0100

    Add doxygen documentation

--- src/main.c
+++ src/main.c
@@ -6,6 +6,16 @@
 
 #include <Windows.h>
 #include "core/interface/program.h"
+
+/**
+ * \brief Provides an entrypoint into the application.
+ * 
+ * \param hInstance handle to the instance of the application.
+ * \param hPrevInstance previous 16-bit instance; not used.
+ * \param lpCmdLine ascii version of the command line; not used.
+ * \param nShowCmd how the application is shown.
+ * \return int - the exit code for the application.
+ */
 int __stdcall WinMain(
     HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
