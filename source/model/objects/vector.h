01c43bd2d9516e50a3a75504debbedc9159a5cb5
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-08-02 17:57:02 +0100

    Add prototypes

--- source/model/objects/vector.h
+++ source/model/objects/vector.h
@@ -1,6 +1,8 @@
 #ifndef NEON_VECTOR_H
 #define NEON_VECTOR_H
 
+#include "../utilities/position.h"
+
 typedef struct _NeonVectorItem
 {
     NeonPosition pos;
@@ -17,10 +19,12 @@ NeonVector NeonNewVector(const int size);
 
 int NeonGenerateNewSize(const int currentSize);
 
+void NeonReSizeVector(const NeonVector *vec);
+
 int NeonIsEmpty(const NeonVector *vec);
 
 int NeonIsFull(const NeonVector *vec);
 
-int NeonFreeVector(NeonVector *vec);
+void NeonFreeVector(NeonVector *vec);
 
 #endif
