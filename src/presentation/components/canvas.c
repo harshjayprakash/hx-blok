238462b382882a6046c857bff4fbf55aded2d218
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-08-11 11:50:17 +0100

    Implement obstructables to gui

--- src/presentation/components/canvas.c
+++ src/presentation/components/canvas.c
@@ -6,18 +6,22 @@
 
 #include "canvas.h"
 #include "../objects/block.h"
+#include "../objects/obstructables.h"
 
 void NeonInitCanvasComponent(void)
 {
     NeonInitBlockObject();
+    NeonInitObstructables();
 }
 
 void NeonRenderCanvasComponent(HDC displayContext)
 {
     NeonRenderBlock(displayContext);
+    NeonRenderObstructables(displayContext);
 }
 
 void NeonFreeCanvasComponent(void)
 {
     NeonFreeBlockObject();
+    NeonFreeObstructables();
 }
--- src/presentation/objects/obstructables.c
+++ src/presentation/objects/obstructables.c
@@ -1,8 +1,11 @@
 #include "obstructables.h"
+#include "../../model/utility/vector.h"
+
+static NeonVector mObstructableSquares = { 0 };
 
 void NeonInitObstructables(void)
 {
-
+    mObstructableSquares = NeonCreateVector(100);
 }
 
 void NeonRenderObstructables(HDC displayContext)
@@ -12,5 +15,5 @@ void NeonRenderObstructables(HDC displayContext)
 
 void NeonFreeObstructables(void)
 {
-    
+    NeonDestroyVector(&mObstructableSquares);
 }
