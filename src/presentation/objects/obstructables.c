bbfc9489c1374bf6f08b8acb3863fc54a2febcc1
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-08-11 12:08:07 +0100

    Set default values to vector

--- src/presentation/objects/obstructables.c
+++ src/presentation/objects/obstructables.c
@@ -6,6 +6,14 @@ static NeonVector mObstructableSquares = { 0 };
 void NeonInitObstructables(void)
 {
     mObstructableSquares = NeonCreateVector(100);
+
+    for (int index = 0; index < mObstructableSquares.max; index++)
+    {
+        NeonCopyPosition(
+            &(NeonGetNodeAsPointer(&mObstructableSquares, index)->position), 
+            NeonCreatePosition(-1, -1)
+        );
+    }
 }
 
 void NeonRenderObstructables(HDC displayContext)
